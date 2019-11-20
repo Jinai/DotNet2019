d5fce3274fc9af639a5c13125560b5d6c0cb7ff5
jinai <jinai.git@gmail.com>
2019-11-20 03:58:04 +0100

    Add use case (participant)

---
+++ SandwichManagementSystem/SMS.BusinessLogic/UseCases/Participant/Participant - AfficherMenu.cs
@@ -0,0 +1,16 @@
+﻿using SMS.BusinessLogic.Extensions;
+using SMS.Shared;
+using SMS.Shared.BTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SMS.BusinessLogic.UseCases.Participant
+{
+    public partial class Participant
+    {
+        public List<SandwichBTO> AfficherMenu(string supplier, Language lang)
+        {
+            return SandwichRepo.GetAll().Select(x => x.ToBTO(lang)).ToList();
+        }
+    }
+}
---
+++ SandwichManagementSystem/SMS.BusinessLogic/UseCases/Participant/Participant - CTOR.cs
@@ -0,0 +1,18 @@
+﻿using SMS.BusinessLogic.Domain;
+using SMS.DataLayer;
+using System;
+
+namespace SMS.BusinessLogic.UseCases.Participant
+{
+    public partial class Participant
+    {
+        public IRepository<Sandwich, int> SandwichRepo { get; }
+        public IRepository<Ingredient, int> IngredientRepo { get; }
+
+        public Participant(IRepository<Sandwich, int> sandwichRepo, IRepository<Ingredient, int> ingredientRepo)
+        {
+            SandwichRepo = sandwichRepo ?? throw new ArgumentNullException(nameof(sandwichRepo));
+            IngredientRepo = ingredientRepo ?? throw new ArgumentNullException(nameof(ingredientRepo));
+        }
+    }
+}
